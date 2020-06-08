16713d63630dffca8dd8395171511defe2105b8a
jiuker <guozhi.li@dmall.com>
2020-06-08 17:43:27 +0800

    pre to syncjs

--- src/server/router.rs
+++ src/server/router.rs
@@ -5,11 +5,9 @@ pub mod my_router {
     
 
     pub async fn syncjs(req: HttpRequest) -> Result<HttpResponse, Error> {
-        println!("REQ: {:?}", req);
         let mut file = File::open("./res/regexp/js/sync.js").expect("没有读取到文件");
         let mut buf = vec![];
         let _ = file.read_to_end(&mut buf).expect("读取错误");
-        println!("{}",buf.len());
         Ok(HttpResponse::Ok().body(buf))
     }
 }
