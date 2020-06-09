2bec44ee22f6260ab32e18acc01eb6bcc6233c7a
jiuker <guozhi.li@dmall.com>
2020-06-09 17:46:13 +0800

    pre to syncjs

--- src/server/router.rs
+++ src/server/router.rs
@@ -8,12 +8,12 @@ pub mod my_router {
     use crate::config::config::{YCONF, COMMON, SINGAL, YConfig};
     use std::ops::Add;
 
-    pub async fn syncjs(req: HttpRequest) -> Result<HttpResponse, Error> {
+    pub async fn syncjs(_req: HttpRequest) -> Result<HttpResponse, Error> {
         let mut file = File::open("./res/regexp/js/sync.js").expect("没有读取到文件");
         let mut buf = vec![];
         let _ = file.read_to_end(&mut buf).expect("读取错误");
-        let mut common_c:MutexGuard<HashMap<String,Regex>> = COMMON.lock().unwrap();
-        let mut singal_c:MutexGuard<HashMap<String,Regex>> = SINGAL.lock().unwrap();
+        let _common_c:MutexGuard<HashMap<String,Regex>> = COMMON.lock().unwrap();
+        let singal_c:MutexGuard<HashMap<String,Regex>> = SINGAL.lock().unwrap();
         let mut will_insert_regs = "".to_string();
         for (value,reg) in singal_c.clone(){
             will_insert_regs = will_insert_regs.add(format!("   this.regexps.push{}\r\n","({").as_ref());
@@ -25,7 +25,7 @@ pub mod my_router {
         Ok(HttpResponse::Ok().body(bufStr))
     }
 
-    pub async fn testHtml(req: HttpRequest) -> Result<HttpResponse, Error> {
+    pub async fn testHtml(_req: HttpRequest) -> Result<HttpResponse, Error> {
         Ok(HttpResponse::Found().header("LOCATION","./res/sample/js/test.html").finish())
     }
 }
