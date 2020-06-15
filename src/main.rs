7724924ebd6980ed3bf85c9563c9581474520304
jiuker <guozhi.li@dmall.com>
2020-06-15 14:29:19 +0800

    修改获取方式

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
 use actix_web::{middleware, web, App, Error as AWError, HttpResponse, HttpServer};
 
 extern crate ycss_rs;
-use std::thread::{spawn};
 use actix_files as fs;
+use actix_web::middleware::Logger;
+use std::convert::TryFrom;
+use std::env::set_var;
+use std::thread::spawn;
 use ycss_rs::repl::repl::Repl;
 use ycss_rs::repl::vue::VueRepl;
-use std::convert::TryFrom;
-use ycss_rs::run::runner::{Runner, FileType};
-use std::env::set_var;
-use actix_web::middleware::Logger;
+use ycss_rs::run::runner::{FileType, Runner};
 use ycss_rs::server::router::my_router;
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    spawn(move||{
-        handle()
-    });
+    spawn(move || handle());
     set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
-    HttpServer::new(||{
+    HttpServer::new(|| {
         App::new()
             .wrap(Logger::default())
             .service(web::resource("/res/regexp/js/sync.js").to(my_router::syncjs))
@@ -30,54 +28,61 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await
 }
-fn handle(){
-    println!("ycss-rs start
+fn handle() {
+    println!(
+        "ycss-rs start
             go....
                 go...
                     go...
-    ");
+    "
+    );
     let run = Runner::new("./res/config/config.json");
-    run.add_dir_watch(vec!["./res/config".to_string()],".json".to_string(),FileType::Config("".to_string())).unwrap();
+    run.add_dir_watch(
+        vec!["./res/config".to_string()],
+        ".json".to_string(),
+        FileType::Config("".to_string()),
+    )
+    .unwrap();
     let run_c = run.clone();
-    spawn(move||{
+    spawn(move || {
         run_c.watch().unwrap();
     });
-    loop{
-        match run.receiver.lock().unwrap().recv().unwrap(){
-            FileType::Config(path)=>{
-                run.load_config(path.as_str()).unwrap()
-            },
-            FileType::Normal(path)=>{
+    while let file = run.receiver.lock().unwrap().recv().unwrap() {
+        match file {
+            FileType::Config(path) => run.load_config(path.as_str()).unwrap(),
+            FileType::Normal(path) => {
                 // 不是配置文件变动
-                println!("get {} changed!",path);
-                let mut rep:VueRepl = Repl::new(path.to_owned());
-                match rep.init().and_then(
-                    |_| rep.get_class().and_then(
-                        |cls| rep.get_new_css(cls).and_then(
-                            |new_css| rep.get_old_css().and_then(
-                                |old_css| {
-                                    if old_css==""{
-                                        return Err(Box::from(format!("not find the auto css contain!forget?[{}]",path)));
-                                    }
-                                    if !rep.is_same(new_css.clone(),old_css.clone()){
-                                        rep.write(new_css.clone(),old_css.clone())?;
-                                    }else{
-                                        return Err(Box::from("is the same! do nothing！"));
-                                    };
-                                    Ok(())
+                println!("get {} changed!", path);
+                let mut rep: VueRepl = Repl::new(path.to_owned());
+                match rep.init().and_then(|_| {
+                    rep.get_class().and_then(|cls| {
+                        rep.get_new_css(cls).and_then(|new_css| {
+                            rep.get_old_css().and_then(|old_css| {
+                                if old_css == "" {
+                                    return Err(Box::from(format!(
+                                        "not find the auto css contain!forget?[{}]",
+                                        path
+                                    )));
                                 }
-                            )
-                        )
-                    )
-                ){
-                    Ok(_d)=>{
+                                if !rep.is_same(new_css.clone(), old_css.clone()) {
+                                    rep.write(new_css.clone(), old_css.clone())?;
+                                } else {
+                                    return Err(Box::from("is the same! do nothing！"));
+                                };
+                                Ok(())
+                            })
+                        })
+                    })
+                }) {
+                    Ok(_d) => {
                         println!("handle file success done!");
-                    },
-                    Err(e)=>{
-                        println!("{}",e);
+                    }
+                    Err(e) => {
+                        println!("{}", e);
                     }
                 };
             }
+            _ => {}
         }
     }
 }
